0131b83668a08fdeff1900973c38c6dc82c0d30e
TravisAdamson <tndadamson@gmail.com>
2023-05-25 09:08:05 -0700

    Done

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Displays numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+		printf("%d", n);
+	}
+	putchar('\n');
+	return (0);
+}
