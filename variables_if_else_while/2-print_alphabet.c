2c3216bcbf502f8a13e27703d2812d9ea5a69811
TravisAdamson <tndadamson@gmail.com>
2023-05-25 08:04:19 -0700

    Done

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Displays lowercase alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char C;
+
+	for(C = 'a' ; C <= 'z' ; C++) 
+	{
+		putchar(C);
+	}
+	putchar('\n');
+	return (0);
+}
