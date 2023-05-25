727384c65b9911b691905688965d0df6ce297f2a
TravisAdamson <tndadamson@gmail.com>
2023-05-25 09:55:05 -0700

    Done

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Displays numbers as characters
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48 ; n < 58 ; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
