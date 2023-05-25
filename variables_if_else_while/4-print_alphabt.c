6df9bf8fcb55f1dadf138385c4fca81513d45e85
TravisAdamson <tndadamson@gmail.com>
2023-05-25 08:38:57 -0700

    D

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Displays lowercase alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char C;
+
+	for (C = 'a' ; C <= 'z' ; C++)
+	{
+		if (( C != 'e' ) && ( C != 'q' ))
+			putchar(C);
+	}
+	putchar('\n');
+	return (0);
+}
