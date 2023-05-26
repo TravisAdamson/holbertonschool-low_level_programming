9ba6146c1fbdff8b80b8ed27730f48b2562543ea
TravisAdamson <tndadamson@gmail.com>
2023-05-26 08:45:52 -0700

    De

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet - prints the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int b;
+
+	for (b = 0; b <= 10; i++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+	}
+	_putchar('\n');
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,2 +1,2 @@
 int _putchar(char);
-void print_alphabet(void);
+void print_alphabet_10x(void);
