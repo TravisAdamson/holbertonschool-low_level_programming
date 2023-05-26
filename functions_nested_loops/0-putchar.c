b8366437bf211b596130869a85e1d52560cec6ee
TravisAdamson <tndadamson@gmail.com>
2023-05-26 07:02:33 -0700

    Check

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Calls the function _putchar to print with
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	char b = b;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+		_putchar(b);
+	}
+	return (0);
+}
---
+++ functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#include "_putchar.c"
+
+/**
+ * main - Calls the function _putchar to print with
+ *
+ * Return: Always 0 (Success)
+ */
+int _putchar(char);
