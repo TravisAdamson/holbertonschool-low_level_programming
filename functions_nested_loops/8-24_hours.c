3577441d8ec855cee3e513f493d254620029642e
TravisAdamson <tndadamson@gmail.com>
2023-05-27 15:01:17 -0700

    GotIt

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -12,17 +12,25 @@ int jack_bauer(void)
 	int s;
 	int t;
 	int f2;
+	int m;
 
 	for (f = 48; f < 51; f++)
 	{
-		for (s = 48; s < 52; s++)
+		for (s = 48; s < 58; s++)
 		{
+			if ((f == 50) && (s > 50))
+			{
+				m = 51;
+				s = 58;
+			}
+			else 
+				m = s;
 			for (t = 48; t < 54; t++)
 			{
 				for (f2 = 48; f2 < 58; f2++)
 				{
 					_putchar(f);
-					_putchar(s);
+					_putchar(m);
 					_putchar(58);
 					_putchar(t);
 					_putchar(f2);
