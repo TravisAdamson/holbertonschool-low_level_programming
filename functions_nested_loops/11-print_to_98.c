d3f83a4029da326d5d9b2097313c5f4d78fc7ca7
TravisAdamson <tndadamson@gmail.com>
2023-05-29 10:07:24 -0700

    Finally

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -11,14 +11,14 @@ void print_to_98(int n)
 {
 	int i;
 
-	if( n < 99 )
+	if (n < 99)
 	{
 		for (i = n; i < 99; i++)
 		{
 			if (i != 98)
 				printf("%d, ", i);
 			else
-				printf("%d",i);
+				printf("%d", i);
 		}
 	}
 	else
