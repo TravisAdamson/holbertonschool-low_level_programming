63655944a9c571467aff551fc3a005c6a1984cb2
TravisAdamson <tndadamson@gmail.com>
2023-05-30 06:04:48 -0700

    Advanced

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,64 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - Prints 9-times table
+ *
+ * Return: Always returns 0
+ */
+void print_times_table(int n)
+{
+	int total;
+	int i;
+	int b;
+	int fd;
+	int ld;
+	int md;
+
+	if ((n < 16) && (n >= 0))
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (b = 0; b <= n; b++)
+			{
+				total = (i * b);
+				if (total > 99)
+				{
+					md = ((total / 10) % 10);
+					fd = (total / 100);
+					ld = (total % 10);
+					_putchar(32);
+					_putchar(fd + 48);
+					_putchar(md + 48);
+					_putchar(ld + 48);
+					if (b != n)
+						_putchar(44);
+				}
+				else if (total > 9)
+				{
+					ld = (total % 10);
+					fd = (total / 10);
+					_putchar(32);
+					_putchar(32);
+					_putchar(fd + 48);
+					_putchar(ld + 48);
+					if (b != n)
+						_putchar(44);
+				}
+				else
+				{
+					if (b != 0)
+					{
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(total + 48);
+					if (b != n)
+						_putchar(44);
+				}
+			}
+			_putchar ('\n');
+		}
+	}
+}
