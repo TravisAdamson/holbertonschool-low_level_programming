f36fabf90d681a7aea406e835a57b6a804bd8feb
TravisAdamson <tndadamson@gmail.com>
2023-05-31 07:56:33 -0700

    Done

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints numbers 1-100 with substitutions
+ *
+ * Return: Always returns 0
+ */
+int main(void)
+{
+	int i;
+	int b;
+	int c;
+
+	for (i = 1; i <= 100; i++)
+	{
+		b = i % 3;
+		c = i % 5;
+		if ((b = 0) && (c = 0))
+			printf("FizzBuzz ");
+		else if (b = 0)
+			printf("Fizz ");
+		else if (c = 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	_putchar('\n');
+}
