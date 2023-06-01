d43675eac6a318f632472f6a112a449fe3134fc6
TravisAdamson <tndadamson@gmail.com>
2023-06-01 06:17:32 -0700

    Done?

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Prints the passed string
+ * @str: String to be be printed
+ *
+ * Return: No Return Value
+ */
+void _puts(char *str)
+{
+	int l = 0;
+	int i;
+	char prstr[] = *str;
+
+	l = _strlen(*str);
+	for (i = 0; i <= l; i++)
+		_putchar(prstr[i]);
+	_putchar('\n');
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include "2-strlen.c"
+
 int _putchar(char);
-int _strlen(char *s);
+int _puts(char *str);
 
 #endif /* MAIN_H */
