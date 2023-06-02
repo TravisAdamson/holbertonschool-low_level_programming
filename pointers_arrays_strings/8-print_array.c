bd50b80178dab95f9d5fc714cac44302ea54a5c0
TravisAdamson <tndadamson@gmail.com>
2023-06-02 07:11:46 -0700

    Done

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -15,8 +15,8 @@ void print_array(int *a, int n)
 	for (i = 0; i <= n; i++)
 	{
 		if (i != n)
-			printf("%d, ", *a[n]);
+			printf("%d, ", a[i]);
 		else
-			printf("%d", *a[n]);
+			printf("%d", a[i]);
 	}
 }
