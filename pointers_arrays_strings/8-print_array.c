92e72046a5fe1d4d317910510bcf80a1b953b737
TravisAdamson <tndadamson@gmail.com>
2023-06-02 07:00:11 -0700

    Done

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - Prints an aray using a given number of elements
+ * @a: The array to be printed
+ * @n: The number of elements in the array
+ *
+ * Return: No Return Value
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (i != n)
+			printf("%d, ", a[n]);
+		else
+			printf("%d", a[n]);
+	}
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,6 +2,6 @@
 #define MAIN_H
 
 int _putchar(char);
-void puts_half(char *str);
+void print_array(int *a, int n);
 
 #endif /* MAIN_H */
