c9d31200445e70bdcbf3ce280d314a4365dd5d25
TravisAdamson <tndadamson@gmail.com>
2023-06-07 12:10:15 -0700

    Testing

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -17,8 +17,8 @@ int _strcmp(char *s1, char *s2)
 	r = 0;
 	while ((s1[i] != '\0') && (s2[i] != '\0'))
 	{
-		if (s1 != s2)
-			r = s1[i] - s2[i];
+		if (s1[i] != s2[i])
+			r = r + s1[i] - s2[i];
 		i++;
 	}
 	return (r);
