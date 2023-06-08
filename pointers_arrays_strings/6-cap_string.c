1ea0c0bca185480870c1a7672633978c956926ec
TravisAdamson <tndadamson@gmail.com>
2023-06-08 06:57:23 -0700

    Done

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *cap_string - Makes all words capitalized
+ * @str: Pointer to string to be converted
+ *
+ * Return: Converted string
+ */
+char *cap_string(char *str)
+{
+	int i;
+	int c;
+	char t;
+	char d[] = "32,33,34,40,41,44,46,59,63,123,125"
+
+	i = 0;
+	c = 0;
+	while (str[i] != '\0')
+	{
+		t = str[i];
+		for (l = 0; l < 11; l++)
+		{
+			if ((t == d[l]) || (t = '\n') || (t = '\t'))
+			{
+				c = 1;
+				l = 11;
+			}
+		}
+		if ((c = 1) && (t > 96) && (t < 123))
+		{
+			str[i] = (t - 32);
+			c = 0;
+		}
+		i++;
+	}
+	return (str);
+}
