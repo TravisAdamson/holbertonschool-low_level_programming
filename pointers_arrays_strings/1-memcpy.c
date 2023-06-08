29dd71d04ca32c9b2505bd068413dff53c67a9a2
TravisAdamson <tndadamson@gmail.com>
2023-06-08 09:28:22 -0700

    Ded

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -14,10 +14,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 
 	i = 0;
-	while ((dest[i] != '\0') && (src[i] != '\0'))
-	{
 		for (i = 0; i < n; i++)
 			dest[i] = src[i];
-	}
 	return (dest);
 }
