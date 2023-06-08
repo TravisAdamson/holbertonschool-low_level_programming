e1aca2dcb2f4c915574a0c0563184aab4076a1bf
TravisAdamson <tndadamson@gmail.com>
2023-06-08 05:34:34 -0700

    Brain

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -10,30 +10,13 @@
  */
 void reverse_array(int *a, int n)
 {
-	int l1;
-	int l2;
-	int *t;
+	int i;
+	int t;
 
-	l1 = 0;
-	l2 = 0;
-	while (l1 < n)
+	for (i = 0; i < n / 2; i++)
 	{
-		t = a;
-		l1++;
-		l2++;
-		t++;
-		a++;
-	}
-	while (l1 > 0)
-	{
-		l1--;
-		a--;
-	}
-	while (l1 < n)
-	{
-		a = t;
-		a++;
-		t--;
-		l1++;
+		t = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = t;
 	}
 }
