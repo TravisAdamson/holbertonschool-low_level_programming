83801c0b6cfee252c6b0a8f82fe3648798a2475a
TravisAdamson <tndadamson@gmail.com>
2023-06-07 17:39:21 -0700

    Done

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - Reverses the passed array
+ * @a: Array to be be reversed
+ * @n: Length of the array
+ *
+ * Return: No Return Value
+ */
+void reverse_array(int *a, int n)
+{
+	int l1;
+	int l2;
+	int *t;
+
+	l1 = 0;
+	l2 = 0;
+	while (l1 < n)
+	{
+		t[l1] = a[l1];
+		l1++;
+	}
+	while (l2 < n)
+	{
+		t[l1] = a[l2];
+		l1--;
+		l2++;
+	}
+}
