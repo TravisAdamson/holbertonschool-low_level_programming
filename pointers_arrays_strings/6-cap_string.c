73eb9be58010fd75abd5ed299848957b012a14c1
TravisAdamson <tndadamson@gmail.com>
2023-06-08 07:24:31 -0700

    Done

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -14,6 +14,7 @@ char *cap_string(char *str)
 	int l;
 	char t;
 	char d[] = {32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 125, '\t', '\n'};
+	
 	i = 0;
 	c = 0;
 	l = 0;
@@ -27,7 +28,7 @@ char *cap_string(char *str)
 				c = 1;
 			}
 		}
-		if ((c = 1) && (t > 96) && (t < 123))
+		if ((c == 1) && (t > 96) && (t < 123))
 		{
 			str[i] = (t - 32);
 			c = 0;
