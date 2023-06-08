a1f31986bbe845aabad0feec4293fa89fff878f6
TravisAdamson <tndadamson@gmail.com>
2023-06-08 05:52:47 -0700

    Test

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - Makes all leters uppercase
+ * @str: Pointer to string to be converted
+ *
+ * Return: Converted string
+ */
+char string_toupper(char *str)
+{
+	int i;
+	char t;
+
+	while (str[i] != '\0')
+	{
+		t = str[i];
+		if ((t > 96) && (t < 123))
+			str[i] = (t - 32);
+		i++;
+	}
+	return(str);
+}
