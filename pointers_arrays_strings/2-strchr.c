b7f1add9c65121b2c6d39b2b7a4d8644d35c053c
TravisAdamson <tndadamson@gmail.com>
2023-06-08 09:39:29 -0700

    Done

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strchr - Locates a char in a passed string
+ * @s: String to search
+ * @c: Character to locate
+ *
+ * Return: Pointer to charcter or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			*p = s[i];
+			return (p);
+		}
+		i++;
+	}
+	return (NULL);
+}
