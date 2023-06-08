50239596cf720bcba284734176001ca6c8690052
TravisAdamson <tndadamson@gmail.com>
2023-06-08 08:50:52 -0700

    Done

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_memset - Directly modify memory based on past arguements
+ * @s: Pointer to modified
+ * @b: Is what to put in memory
+ * @n: Is number of bytes to change
+ *
+ * Return: Pointer to Memory Address of s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	i = 0;
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (&s);
+}
