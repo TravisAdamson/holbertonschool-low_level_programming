ee57af4164c2d82c4497d0dcb95693b8587c1aa6
TravisAdamson <tndadamson@gmail.com>
2023-06-09 08:46:54 -0700

    Done

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -13,15 +13,14 @@ void print_diagsums(int *a, int size)
 	int t1;
 	int t2;
 	int i;
-	int *p[][] = a;
 
 	t1 = 0;
 	t2 = 0;
 	i=0;
 	for (i = 0; i < size; i++)
 	{
-		t1 = p[i][i] + t1;
-		t2 = p[size - i][size - i] + t2;
+		t1 = a[i * i] + t1;
+		t2 = a[(size - i) * (size - i)] + t2;
 	}
 	printf("%d, %d", t1, t2);
 }
