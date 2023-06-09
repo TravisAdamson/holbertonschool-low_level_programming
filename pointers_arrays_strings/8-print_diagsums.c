d9a8660f766ebcd723e2d84e57c95fde3e73153b
TravisAdamson <tndadamson@gmail.com>
2023-06-09 07:55:14 -0700

    Done

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - Print the diagnal sums of a given array
+ * @a: Array to be calculated
+ * @size: size of the array
+ *
+ * Return: No return
+ */
+void print_diagsums(int *a, int size)
+{
+	int t1;
+	int t2;
+	int i;
+
+	t1 = 0;
+	t2 = 0;
+	i=0;
+	for (i = 0; i < size; i++)
+	{
+		t1 = a[i][i] + t1;
+		t2 = a[size - i][size - i] + t2;
+	}
+	printf("%d, %d", t1, t2);
+}
