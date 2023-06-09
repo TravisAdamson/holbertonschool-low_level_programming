31f3c6294ce69382d29b4763462c835875778130
TravisAdamson <tndadamson@gmail.com>
2023-06-09 07:17:54 -0700

    Done

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - Print the chessboard
+ * @a: Array with content
+ *
+ * Return: No return
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int i2;
+
+	i = 0;
+	i2 = 0;
+	for (i = 0; i < 8; i++)
+	{
+		for (i2 = 0; i2 < 8; i2++)
+		{
+			_putchar(a[i2][i]);
+		}
+	}
+}
