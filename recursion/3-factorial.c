043c01f978575dd6c777048878b60adf12aa34b7
TravisAdamson <tndadamson@gmail.com>
2023-06-14 07:58:15 -0700

    Done

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -11,10 +11,7 @@ int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
-	else
-	{
-		if (n != 0)
-			return (factorial(n / 2));
-	}
-	return (0);
+	if (n >0)
+		return (n * factorial(n - 1));
+	return (1);
 }
