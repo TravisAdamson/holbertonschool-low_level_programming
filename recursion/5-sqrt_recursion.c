79e6245fda4fcb48e77c6cd83429c15798dee524
TravisAdamson <tndadamson@gmail.com>
2023-06-14 08:31:15 -0700

    Done

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -3,15 +3,30 @@
 
 /**
  * _sqrt_recursion - Uses recursion to get sqrt of n
+ * calc - determine sqrt
  * @n: Number to calculate
+ * @endval: Result
  *
- * Return: Sqrt
+ * Return: Result
  */
+
+int calc(int n, int endval)
+{
+	if (endval * endval == n)
+	{
+		return (endval);
+	}
+	else if (endval * endval > n)
+	{
+		return (-1);
+	}
+	else
+		return (calc(n, endval + 1));
+}
+
 int _sqrt_recursion(int n)
 {
-	if (n / 2 != 0)
+	if (n < 0)
 		return (-1);
-	else
-		return (_sqrt_recursion(n/2));
-	return (1);
+	return (calc(n,0));
 }
