fee8d07ff36668720b58e3466f3a008cf1056cb0
TravisAdamson <tndadamson@gmail.com>
2023-06-14 07:04:51 -0700

    Done

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - Uses recursion to get strlen
+ * @s: What to evaluate
+ *
+ * Return: Strlen
+ */
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+	if (*s != '\0')
+		i = _strlen_recursion(*s + 1);
+	else
+		return (i);
+}
