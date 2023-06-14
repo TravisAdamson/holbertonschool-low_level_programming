0595efcfdd92c8f4c34dde0e6219ede1de660e97
TravisAdamson <tndadamson@gmail.com>
2023-06-14 06:15:07 -0700

    done

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - Uses recursion to print reverse
+ * @s: What to print
+ *
+ * Return: No Return
+ */
+void _print_rev_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+	if ((*s != '\0') && (i != 0))
+	{
+		_putchar(*s);
+		s--;
+		_print_rev_recursion(s);
+	}
+	else
+	{
+		s++;
+		if (*s == '\0')
+			i = 1;
+		_print_rev_recursion(s);
+}
