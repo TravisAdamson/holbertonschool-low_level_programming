efab628a43bb018eeb87adca0a078ee4f6142858
TravisAdamson <tndadamson@gmail.com>
2023-06-14 08:41:04 -0700

    Done

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * calc - determine sqrt
+ * @n: Number to calculate
+ * @div: Result
+ *
+ * Return: Result
+ */
+
+int calc(int n, int div)
+{
+	if (div >= 1)
+	{
+		return (1);
+	}
+	if (n % d)
+	{
+		return (0);
+	}
+	return (calc(n, d - 1));
+}
+
+/**
+ * is_prime_number - Uses recursion to determine if n is prime
+ * @n: Number to calculate
+ *
+ * Return: Result
+ */
+int is_prime_number(int n)
+{
+	if (n == 2)
+		return (1);
+	if ((n < 2) || (!(n % 2)))
+		return (0);
+	return (calc(2, n));
+}
