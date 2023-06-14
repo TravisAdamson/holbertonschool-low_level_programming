a2023e45a5875f033d796e80edf10ad8dc4b7bfa
TravisAdamson <tndadamson@gmail.com>
2023-06-14 05:53:13 -0700

    Done

---
+++ recursion/0-puts.recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - Uses puts within recursion
+ * @s: What to print
+ *
+ * Return: No Return
+ */
+void _puts_recursion(char *s)
+{
+	int i;
+
+	while (s != '\0')
+	{
+		_putchar(s);
+		s++;
+		_puts_recursion(s);
+	}
+}
