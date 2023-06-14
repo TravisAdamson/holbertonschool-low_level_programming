50cc0540fe91b6618a3e3312dc48e016b504fc48
TravisAdamson <tndadamson@gmail.com>
2023-06-14 07:47:52 -0700

    Done

---
+++ recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * factorial - Uses recursion to get factorials
+ * @n: Number to evaluate
+ *
+ * Return: Factorial values
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else
+		return (n / factorial(n);
+}
