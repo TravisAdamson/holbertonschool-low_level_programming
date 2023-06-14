b297f8d0b22428462ae4242f1159c64f750e2350
TravisAdamson <tndadamson@gmail.com>
2023-06-14 06:32:45 -0700

    Done

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -11,9 +11,8 @@ void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_putchar(*s);
+		_putchar(*s + _print_rev_recursion(s + 1));
 		++ s;
-		_print_rev_recursion(s);
 	}
 	else
 		_putchar('\n');
