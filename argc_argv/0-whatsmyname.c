5875ff09da5fa968e8c74f597ed8e101d32cc57e
TravisAdamson <tndadamson@gmail.com>
2023-06-15 05:45:37 -0700

    Done

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -3,11 +3,14 @@
 
 /**
  * main - Prints the program name
+ * @argc: Count of arguements
+ * @argv: Vector of arguements
  *
  * Return: Always Return 0
  */
-int main(char **argv)
+int main(int argc, char *argv[])
 {
-	printf("%c\n", *argv[0]);
+	(void) argc;
+	printf("%s\n", argv[0]);
 	return (0);
 }
