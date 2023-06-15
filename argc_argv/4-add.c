3ad13049c7a3bb838bc1d1a6ff08b0dbc62c4bbe
TravisAdamson <tndadamson@gmail.com>
2023-06-15 07:27:25 -0700

    Done

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -14,17 +14,14 @@ int main(int argc, char *argv[])
 {
 	int i;
 	int t;
-	int m;
 
 	t = 0;
-	m = 0;
 	for (i = 0; i < argc; i++)
 	{
 		printf("Testing");
 		if (isdigit(argv[i]) > 0)
 		{
 			t = t + atoi(argv[i]);
-			m = 1;
 		}
 	}
 	if (t == 0)
