446fdd37dec7a574995a5b3d62bad1af636dd1c3
TravisAdamson <tndadamson@gmail.com>
2023-06-15 07:15:14 -0700

    Done

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 	t = 0;
 	for (i = 0; i < argc; i++)
 	{
-		if (isdigit(atoi(argv[i])))
+		if (isdigit(argv[i]))
 			t = t + atoi(argv[i]);
 	
 		if ((t > 0) && (i == argc - 1))
