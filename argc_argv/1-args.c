11ef1e38de30aace93cc0c5cb2498b417c055f94
TravisAdamson <tndadamson@gmail.com>
2023-06-15 05:47:34 -0700

    Done

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the program name
+ * @argc: Count of arguements
+ * @argv: Vector of arguements
+ *
+ * Return: Always Return 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argvc;
+	printf("%d\n", argc);
+	return (0);
+}
