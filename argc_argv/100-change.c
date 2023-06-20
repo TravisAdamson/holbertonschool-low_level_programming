b5631ab7d814cd17dcf84ac97b2407732446998f
TravisAdamson <tndadamson@gmail.com>
2023-06-20 05:33:38 -0700

    Done

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -4,18 +4,14 @@
 #include <ctype.h>
 
 /**
- * reduce_to_cents - Calculates the minimum number of coins
+ * main - Calculates the minimum number of coins
  * @argc: Count of arguements
  * @argv: Vector of arguements
  *
  * Return: The minimum number of coins
  */
-int reduce_to_cents(int argc, char *argv[])
+int main(int argc, char *argv)
 {
-	int q = 25;
-	int d = 10;
-	int n = 5;
-	int p = 1;
 	int s = 0;
 
 	if (argc == 2)
@@ -26,20 +22,36 @@ int reduce_to_cents(int argc, char *argv[])
 			printf("0\n");
 			return (0);
 		}
-		if (s > q)
-			return ( 1 + reduce_to_cents(s - 25));
-		else if (s > d)
-			return ( 1 + reduce_to_cents(s - 10));
-		else if (s > n)
-			return ( 1 + reduce_to_cents(s - 5));
-		else if (s > p)
-			return ( 1 + reduce_to_cents(s - 1));
-		else
-			return (0);
+		return (reduce_to_cents(s));
 	}
 	else
 	{
 		printf("Error\n");
 		return (1);
 	}
+}
+/**
+ * reduce_to_cents - Calculates the minimum number of coins
+ * @s: The value to be examined
+ *
+ * Return: The minimum number of coins
+ */
+int reduce_to_cents(int s)
+{
+	int q = 25;
+	int d = 10;
+	int n = 5;
+	int p = 1;
+	int s = 0;
+
+	if (s > q)
+		return ( 1 + reduce_to_cents(s - 25));
+	else if (s > d)
+		return ( 1 + reduce_to_cents(s - 10));
+	else if (s > n)
+		return ( 1 + reduce_to_cents(s - 5));
+	else if (s > p)
+		return ( 1 + reduce_to_cents(s - 1));
+	else
+		return (0);
 }
