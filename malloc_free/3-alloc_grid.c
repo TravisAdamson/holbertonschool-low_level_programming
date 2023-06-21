4b7b4c0b95c82b3b43fc4d93ba5ed62fdfab4e45
TravisAdamson <tndadamson@gmail.com>
2023-06-21 11:01:40 -0700

    Done

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -20,12 +20,12 @@ int **alloc_grid(int width, int height)
 	i2 = 0;
 	if ((width <= 0) || (height <= 0))
 		return (NULL);
-	point = malloc(width * sizeof(int *));
+	point = malloc(height * sizeof(int *));
 	if (point == NULL)
 		return (NULL);
-	for (i = 0; i < height; i++)
+	for (i = 0; i <= height; i++)
 	{
-		point[i] = malloc(height * sizeof(int));
+		point[i] = malloc(width * sizeof(int));
 		if (point[i] == NULL)
 		{
 			while (i >= 0)
@@ -33,9 +33,9 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 	}
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
-		for (i2 = 0; i < height; i++)
+		for (i2 = 0; i2 < width; i2++)
 			point[i][i2] = 0;
 	}
 	return (point);
