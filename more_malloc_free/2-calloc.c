091b2f40a7a1983bdff8cfd8092551d2a705d60a
TravisAdamson <tndadamson@gmail.com>
2023-06-22 10:45:27 -0700

    Done

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * *_calloc - Allocates an array with malloc
+ * @nmemb: Array
+ * @size: Size of the given array
+ *
+ * Return: Pointer to new memmory location
+ */
+char *_caloc(unsigned int nmemb, unsigned int size)
+{
+	char *point;
+
+	if ((nmemb == 0) || (size = 0))
+		return (NULL);
+	point = malloc(nmemb, size);
+	if (point == NULL)
+		return (NULL);
+	return (point);
+}
