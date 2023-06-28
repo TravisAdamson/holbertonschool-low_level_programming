ce7d67abafdc4ddf136e52c7e53a9af42eab39ff
TravisAdamson <tndadamson@gmail.com>
2023-06-28 09:22:21 -0700

    Done

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -14,15 +14,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	int l1;
 	int l2;
+	int i;
 	char *n2 = name;
 	char *o2 = owner;
 	dog_t *tdog;
 	
 	l1 = 0;
 	l2 = 0;
-	while (name[l1])
+	while (name[l1] != '\0')
 		l1++;
-	while (owner[l2])
+	while (owner[l2] != '\0')
 		l2++;
 	tdog = malloc(sizeof(dog_t));
 	if (tdog == NULL)
@@ -36,6 +37,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		tdog->name[i] = n2[i];
 	tdog->age = age;
 	for (i = 0; i < l2; i++)
-		tdog->owner[i] = owner[i];
+		tdog->owner[i] = o2[i];
 	return (tdog);
 }
