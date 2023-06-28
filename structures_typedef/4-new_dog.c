c531d0fcb396a305244ef30d094a3d84ff6b639f
TravisAdamson <tndadamson@gmail.com>
2023-06-28 07:49:23 -0700

    Done

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -25,29 +25,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 		l2++;
 	tempdog = malloc(sizeof(dog_t));
 	if (tempdog == NULL)
-	{
-		free(tempdog);
 		return (NULL);
-	}
 	tempdog->name = malloc(l1 * sizeof(tempdog->name));
 	if (tempdog == NULL)
-	{
-		free(tempdog->name);
-		free(tempdog);
 		return (NULL);
-	}
 	for (i = 0; i < l1; i++)
 		tempdog->name[i] = name[i];
 	tempdog->age = age;
 	tempdog->owner = malloc(l2 * sizeof(tempdog->owner));
 	if (tempdog == NULL)
-	{
-		free(tempdog->name);
-		free(tempdog->owner);
-		free(tempdog->age);
-		free(tempdog);
 		return (NULL);
-	}
 	for (i = 0; i < l2; i++)
 		tempdog->owner[i] = owner[i];
 	return (tempdog);
