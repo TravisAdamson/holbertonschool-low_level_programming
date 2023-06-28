a66bf9ec9cda92e3192089dd6ca91e831f415bf2
TravisAdamson <tndadamson@gmail.com>
2023-06-28 05:28:37 -0700

    New Dog Struct

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Defines structure dog
+ * @name: The given name for the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * description - Holds the three pieces of data on the dog
+ */
+
+typedef struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
