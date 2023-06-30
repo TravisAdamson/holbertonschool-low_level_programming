92903736b6eacfa3d74d13008ed2f6f8beb4f7ab
TravisAdamson <tndadamson@gmail.com>
2023-06-29 17:32:50 -0700

    I need to learn how to type

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "3-calc.h"
 
 /**
@@ -9,15 +10,16 @@
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-		{'+', op_add},
-		{'-', op_sub},
-		{'*', op_mul},
-		{'/', op_div},
-		{'%', op_mod},
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
 		{NULL, NULL}
 	};
 	int i;
 
+	i = 0;
 	while (i <= 5)
 	{
 		if (*ops[i].op == *s)
