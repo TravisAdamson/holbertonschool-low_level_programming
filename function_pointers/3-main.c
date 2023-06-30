391881aad8ecc46ae825dba4cefbb92224f8b253
TravisAdamson <tndadamson@gmail.com>
2023-06-29 18:51:34 -0700

    Testing

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 	}
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	if (get_op_func(*argv[2]) == NULL)
+	if (get_op_func(argv[2]) == NULL)
 	{
 		printf("Error\n");
 		exit(99);
