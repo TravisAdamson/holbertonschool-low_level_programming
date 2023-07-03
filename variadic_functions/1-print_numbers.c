ed02b2324480c890d3f56f06d028f98185dceb09
TravisAdamson <tndadamson@gmail.com>
2023-07-03 06:24:29 -0700

    Betty

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(lst, int));
 		if ((separator != NULL) && (i < n - 1))
-			printf ("%s", separator);
+			printf("%s", separator);
 	}
 	printf("\n");
 	va_end(lst);
