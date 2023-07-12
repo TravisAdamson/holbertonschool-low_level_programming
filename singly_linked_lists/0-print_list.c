c0c8000b3d359c3a98816517f4eae2cc1b40504b
TravisAdamson <tndadamson@gmail.com>
2023-07-12 07:19:34 -0700

    First 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_list - Prints the elements of the given list
+ * @h: The list to be used
+ *
+ * Return: The number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	for (i = 0; h; i++)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return (i);
+}
