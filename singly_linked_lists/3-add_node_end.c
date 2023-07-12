c61d7d2c6b57792ffd4193bbd182e424051643cd
TravisAdamson <tndadamson@gmail.com>
2023-07-12 08:44:05 -0700

    Number 3

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_node - Adds new nodes
+ * @head: The previous list_t
+ * @str: Value to be added to new list_t
+ *
+ * Return: The address of the new list_t
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	int i, length;
+	char *newvalue;
+	list_t *new, *current;;
+
+	length = 0;
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+		length++;
+	new = *head;
+	newvalue = malloc(length + 1);
+	if (newvalue == NULL)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+		newvalue[i] = str[i];
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		free(newvalue);
+		return (NULL);
+	}
+	new->str = newvalue;
+	new->len = length;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (*head);
+	}
+	current = *head;
+	while (current->next != NULL)
+		current = current->next;
+	current->next = new;
+	return (*head);
+}
