e56797df63ec75f36345bb11acd5c3e7ae9c9ad9
TravisAdamson <tndadamson@gmail.com>
2023-07-12 07:25:38 -0700

    Added quotes

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -16,7 +16,7 @@ size_t print_list(const list_t *h)
 	for (i = 0; h; i++)
 	{
 		if (h->str == NULL)
-			h->str = (nil);
+			h->str = "(nil)";
 		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 	}
