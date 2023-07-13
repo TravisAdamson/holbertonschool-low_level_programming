c2af7a3c72974f40f838f5df060b4d7c9230de63
TravisAdamson <tndadamson@gmail.com>
2023-07-13 05:26:22 -0700

    Try 1 for 0

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * binary_to_uint - Converts a string of 0 and 1s to a uint
+ * @b: the string to convert
+ *
+ * Return: The unsigned int after conversion
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	int length;
+	unsigned int total;
+	unsigned int svalue;
+
+	length = 0;
+	svalue = 1;
+	total = 0;
+	for (i = 0; b; i++)
+		length++;
+	for (i = length - 1; b[i]; i--)
+	{
+		if ((b[i] != 1) && (b[i] != 0))
+			return (0);
+		tempc = ((char) b[i]);
+		tempv = ((unsigned int) tempc - 48);
+		total += tempv * svalue;
+		svalue *= 2;
+	}
+	return (total);
+}
