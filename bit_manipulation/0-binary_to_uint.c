800f85ff957ea09309a7dc2df038dd3b7ebac15a
TravisAdamson <tndadamson@gmail.com>
2023-07-13 05:39:42 -0700

    Try 2 for 0

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -9,6 +9,17 @@
  *
  * Return: The unsigned int after conversion
  */
+
+unsigned int str_to_int(char a)
+{
+	return((unsigned int) a - '0');
+}
+/**
+ * binary_to_uint - Converts a string of 0 and 1s to a uint
+ * @b: the string to convert
+ *
+ * Return: The unsigned int after conversion
+ */
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
@@ -16,19 +27,19 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int total;
 	unsigned int svalue;
 	unsigned int tempv = 0;
-	char tempc = 0;;
 
 	length = 0;
 	svalue = 1;
 	total = 0;
+	if (b == NULL)
+		return (0);
 	for (i = 0; b; i++)
 		length++;
 	for (i = length - 1; b[i]; i--)
 	{
 		if ((b[i] != 1) && (b[i] != 0))
 			return (0);
-		tempc = ((char) b[i]);
-		tempv = ((unsigned int) tempc - 48);
+		tempv = str_to_int(b[i]);
 		total += tempv * svalue;
 		svalue *= 2;
 	}
