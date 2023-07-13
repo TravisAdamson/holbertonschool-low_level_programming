9b4cf4e8ec6f7607ff760768697bf46df37e80ab
TravisAdamson <tndadamson@gmail.com>
2023-07-13 08:50:14 -0700

    Testing 1

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_binary - Prints a number provided in binary
+ * @n: The number to be printed
+ *
+ * Return: No Return
+ */
+void print_binary(unsigned long int n)
+{
+	int test;
+
+	if (n == 0)
+		_putchar('0');
+	test = 0;
+	for (i = sizeof(n) * 8 - 1; i >= 0; i--)
+	{
+		if ((n >> i) & 1)
+			test = 1;
+		if (test == 1)
+		{
+			if (( n >> i) & 1)
+				_putchar(1);
+			else
+				_putchar(0);
+		}
+	}
+
+}
