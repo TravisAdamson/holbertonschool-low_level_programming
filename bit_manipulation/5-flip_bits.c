c7274e16f97414912042117ac64add4a6abeec10
TravisAdamson <tndadamson@gmail.com>
2023-07-14 05:41:20 -0700

    Test for final problem

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * flip_bits - Find the number of bits needed to flip
+ * @n: The starting number
+ * @m: The resulting number
+ *
+ * Return: Bits needed to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int result;
+	unsigned long int i;
+
+	i=0;
+	result = 0;
+	for (i = 8 * sizeof(n) - 1; i >= 0; i--)
+	{
+		if (((n ^ m) >> i) & 1)
+			result++;
+	}
+	return (result);
+}
+
