6436019936ac3b20bc712851602f49ef9ff0a5ff
TravisAdamson <tndadamson@gmail.com>
2023-07-15 08:20:11 -0700

    Test wstat value

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -14,7 +14,6 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	name = open(filename, O_APPEND);
-	printf("%d\n", name);
 	if (name == -1)
 		return (-1);
 	if (text_content == NULL)
@@ -25,6 +24,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	for (length = 0; text_content[length]; length++)
 		;
 	wstat = write(name, text_content, length);
+	printf("%d\n", wstat);
 	if (close(name) == -1)
 		return (-1);
 	if (wstat == -1)
