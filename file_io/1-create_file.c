0b168c7638537902e87bda11136ea318484c4621
TravisAdamson <tndadamson@gmail.com>
2023-07-14 17:52:40 -0700

    Done

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	name = open(filename, O_RDWR | O_CREAT | O_TRUNC | O_NOATIME);
+	name = open(filename, O_WRONLY | O_CREAT | O_TRUNC | S_IWUSR, S_IRUSR);
 	if (name == -1)
 		return (-1);
 	if (text_content == NULL)
