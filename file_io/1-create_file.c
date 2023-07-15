64dbe9daabf0c5dc5f32e42f43235386126fa0af
TravisAdamson <tndadamson@gmail.com>
2023-07-14 17:58:03 -0700

    Trying to fix perm issues

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,7 +13,10 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	name = open(filename, O_WRONLY | O_CREAT | O_TRUNC | S_IWUSR, S_IRUSR);
+	if (access(filename))
+		name = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
+	else
+		name = open(filename, O_RDWR | O_CREAT | O_TRUNC);
 	if (name == -1)
 		return (-1);
 	if (text_content == NULL)
