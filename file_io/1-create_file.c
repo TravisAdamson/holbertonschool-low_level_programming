561fd6223c990f75c7094b9a9cb3e501cbfb7cca
TravisAdamson <tndadamson@gmail.com>
2023-07-15 07:32:20 -0700

    Syntax fix

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	name = open(filename, O_RDWR | O_CREAT | O_TRUNC), 0600;
+	name = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (name == -1)
 		return (-1);
 	if (text_content == NULL)
