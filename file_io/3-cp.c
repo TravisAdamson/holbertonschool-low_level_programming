a8eed233062699e12e60ed7ff21af1e5468ffa4f
TravisAdamson <tndadamson@gmail.com>
2023-07-15 09:33:14 -0700

    More error fixing

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -17,7 +17,7 @@ int main(int ac, char *av[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	ostat = open(av[1], O_RDONLY);
 	if (ostat == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", av[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	nostat = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, Permit);
 	if (nostat == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
