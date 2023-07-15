3d6a9efbf418a0270039105dc935e5127f23a1cd
TravisAdamson <tndadamson@gmail.com>
2023-07-15 07:31:22 -0700

    Testing

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,11 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (access(filename, 2))
-		name = open(filename, O_CREAT | O_TRUNC);
-	else
-		name = open(filename, O_RDWR | O_CREAT | O_TRUNC);
-	_putchar(name);
+	name = open(filename, O_RDWR | O_CREAT | O_TRUNC), 0600;
 	if (name == -1)
 		return (-1);
 	if (text_content == NULL)
