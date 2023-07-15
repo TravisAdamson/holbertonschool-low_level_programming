a727723646b253247df9fc8bc2323c62c238b102
TravisAdamson <tndadamson@gmail.com>
2023-07-15 16:56:48 -0700

    added *

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -11,9 +11,9 @@
  */
 void free_listint2(listint_t **head)
 {
-	if (head == NULL)
+	if (*head == NULL)
 		return;
-	free_listint2(head->next);
-	free(head);
+	free_listint2(*head->next);
+	free(*head);
 	*head = NULL;
 }
