7edb646b659984ac172fd973f14287aea98dd588
TravisAdamson <tndadamson@gmail.com>
2023-07-14 17:34:59 -0700

    Testing typo fix

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,12 +9,11 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int name, total, rstatus;
-	char buffer[BSIZE];
+	int name, total, wstat, length;
 
 	if (filename == NULL)
 		return (-1);
-	name = open(filename, O_RDRW | O_CREAT | O_TRUNC);
+	name = open(filename, O_RDWR | O_CREAT | O_TRUNC);
 	if (name == -1)
 		return (-1);
 	if (text_content == NULL)
