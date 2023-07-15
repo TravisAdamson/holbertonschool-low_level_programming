992ef374e99491c0f2e449dc483dd568be7593b6
TravisAdamson <tndadamson@gmail.com>
2023-07-15 09:23:47 -0700

    Fixed pointer referencing

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -11,31 +11,32 @@ int main(int ac, char *av[])
 {
 	int rstat, ostat, nostat, wstat;
 	char buffer[BSIZE];
+	mode_t Permit = S_IRUSR | S_IWUSR | S_IWGRP | S_IRGRP | S_IROTH
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n", exit(97));
 	ostat = open(av[1], O_RDONLY);
 	if (ostat == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", *av[1], exit(98));
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1], exit(98));
 	nostat = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, perm);
 	if (nostat == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", *av[2], exit(99));
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2], exit(99));
 	rstat = 1;
 	while (rstat)
 	{
 		rstat = read(ostat, buffer, BSIZE);
 		if (rstat == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", *av[1], exit(98));
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1], exit(98));
 		if (rstat > 0)
 		{
 			wstat = write(nostat, buffer, rstat);
 			if (wstat != rstat || wstat == -1)
-				dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", *av[2], exit(99));
+				dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2], exit(99));
 		}
 	}
 	if (close(ostat) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close file %s\n", *av[1], exit(100));
+		dprintf(STDERR_FILENO, "Error: Can't close file %s\n", av[1], exit(100));
 	if (close(nostat) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close file %s\n", *av[2], exit(100));
+		dprintf(STDERR_FILENO, "Error: Can't close file %s\n", av[2], exit(100));
 	return (0);
 }
