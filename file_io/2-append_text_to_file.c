b5c9bbeb939b06203a5dbd4ebe713fc08fb047da
TravisAdamson <tndadamson@gmail.com>
2023-07-15 07:54:12 -0700

    Fixed a typo

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -13,7 +13,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	name = open(filename, O_APPEND | O_EXCEL);
+	name = open(filename, O_APPEND | O_EXCL);
 	if (name == -1)
 		return (-1);
 	if (text_content == NULL)
