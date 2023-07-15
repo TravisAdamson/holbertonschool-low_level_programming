9a52009814eee16bebeb07a37e2669c7ee676a3c
TravisAdamson <tndadamson@gmail.com>
2023-07-14 17:44:30 -0700

    Fixed permissions

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	name = open(filename, O_RDWR | O_CREAT | O_TRUNC);
+	name = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
 	if (name == -1)
 		return (-1);
 	if (text_content == NULL)
@@ -21,7 +21,8 @@ int create_file(const char *filename, char *text_content)
 		close(name);
 		return (1);
 	}
-	for (length = 0; text_content[length]; length++);
+	for (length = 0; text_content[length]; length++)
+		;
 	wstat = write(name, text_content, length);
 	if (close(name) == -1)
 		return (-1);
