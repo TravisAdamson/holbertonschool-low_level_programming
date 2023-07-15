42970154142040e69b9b82cc89631b1e80a3d29f
TravisAdamson <tndadamson@gmail.com>
2023-07-14 18:04:52 -0700

    Testing name value

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -17,6 +17,7 @@ int create_file(const char *filename, char *text_content)
 		name = open(filename, O_CREAT | O_TRUNC);
 	else
 		name = open(filename, O_RDWR | O_CREAT | O_TRUNC);
+	_putchar(name);
 	if (name == -1)
 		return (-1);
 	if (text_content == NULL)
