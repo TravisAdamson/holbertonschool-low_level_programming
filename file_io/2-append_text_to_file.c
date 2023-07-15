f2fdee51cf422d036fe243251448ebc6ad1c92b4
TravisAdamson <tndadamson@gmail.com>
2023-07-15 08:46:15 -0700

    Fixed overwrite issue

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -14,7 +14,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fpoint = fopen(filename, "w+");
+	fpoint = fopen(filename, "a+");
 	if (fpoint == NULL)
 		return (-1);
 	if (text_content == NULL)
