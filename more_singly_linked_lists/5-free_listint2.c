8dab9b923bd820252a78cd63f3718fd93010f96c
TravisAdamson <tndadamson@gmail.com>
2023-07-15 16:53:49 -0700

    Try 1 of 5

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - Free the elements of the given list and sets head to NULL
+ * @head: The list to be freed
+ *
+ * Return: No Return
+ */
+void free_listint2(listint_t **head)
+{
+	if (head == NULL)
+		return;
+	free_listint(head->next);
+	free(head);
+	*head = NULL;
+}
