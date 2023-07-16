234e4e15cdceb004960426e5c058fc0738bfe8d8
TravisAdamson <tndadamson@gmail.com>
2023-07-16 14:26:33 -0700

    Try 1 at 8

---
+++ more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * sum_listint - Finds the sum of the nodes in the given list
+ * @head: The list to add up
+ *
+ * Return: The sum of nodes in the list
+ */
+int sum_listint(listint_t *head)
+{
+	int sum;
+
+	for (sum = 0; head; sum+= head->n)
+		;
+	return (sum);
+}
