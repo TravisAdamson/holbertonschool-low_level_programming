3df615053ccb4e3d34f08b5f499f144abe70e758
TravisAdamson <tndadamson@gmail.com>
2023-07-15 17:30:13 -0700

    Still not functioning

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -13,7 +13,8 @@ void free_listint2(listint_t **head)
 {
 	if (*head == NULL)
 		return;
-	free_listint2(*head->next);
+	**head = *head->next; 
+	free_listint2(**head);
 	free(*head);
 	*head = NULL;
 }
