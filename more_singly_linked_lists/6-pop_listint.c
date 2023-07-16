8b882353c89e86c79472fcb7d68d50eabbc1050c
TravisAdamson <tndadamson@gmail.com>
2023-07-16 13:30:48 -0700

    Try 1 for 6

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * pop_listint - Pops off the head of the given list
+ * @head: The list to remove the head
+ *
+ * Return: The content of the head
+ */
+int pop_listint(listint_t **head)
+{
+	int rvalue;
+	listint_t *temph, *newhead;
+
+	rvalue = head->n;
+	if (head == NULL)
+		return (NULL);
+	newhead = malloc(sizeof(listint_t));
+	if (newhead == NULL)
+		return (NULL);
+	newhead = head->next;
+	head = newhead;
+	free(newhead);
+	return (rvalue);
+}
