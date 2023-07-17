31bb364ebde0592a0834e15c63f784cd7628dd11
TravisAdamson <tndadamson@gmail.com>
2023-07-17 05:38:12 -0700

    Try 1 of 9

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - Adds new node at index
+ * @head: The list to add node to
+ * @idx: The location to add the node to
+ * @n: Value of the node to be added
+ *
+ * Return: The address of new node, or NULL on error
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *temp, *previous;
+
+	if (*head == NULL)
+		return (NULL);
+	previous = *head;
+	temp = malloc(sizeof(listint_t));
+	if (temp == NULL)
+		return (NULL);
+	temp->n = n;
+	if (idx == 0)
+	{
+		temp->next = previous;
+		*head = temp;
+		return (*head);
+	}
+	while (idx > 1)
+	{
+		previous = previous->next;
+		index--;
+		if (!previous)
+		{
+			free(temp);
+			return (NULL);
+		}
+	}
+	temp->next = previous->next;
+	previous->next = temp;
+	return (temp);
+}
