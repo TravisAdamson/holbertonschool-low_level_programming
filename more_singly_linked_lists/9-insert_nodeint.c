c867047244b9151f9f036fd91d98fc9af71d1e41
TravisAdamson <tndadamson@gmail.com>
2023-07-17 05:40:34 -0700

    Fixed variable name

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	while (idx > 1)
 	{
 		previous = previous->next;
-		index--;
+		idx--;
 		if (!previous)
 		{
 			free(temp);
