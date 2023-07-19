e42b5f405ee3973d5a665a25e83b8f36b6d4432b
TravisAdamson <tndadamson@gmail.com>
2023-07-19 11:54:20 -0700

    Try 1

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_distint - Free the elements of the given list
+ * @head: The list to be freed
+ *
+ * Return: No Return
+ */
+void free_dlistint(dlistint_t *head)
+{
+	if (head == NULL)
+		return;
+	free_dlistint(head->next);
+	free(head);
+}
