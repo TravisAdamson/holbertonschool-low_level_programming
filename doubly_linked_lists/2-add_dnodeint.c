1774d1d0a8b2d4ccc2a92c226569f86160b3c8b1
TravisAdamson <tndadamson@gmail.com>
2023-07-19 10:15:17 -0700

    Try 2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -10,7 +10,7 @@
  *
  * Return: The address of the new list_t
  */
-listint_t *add_dnodeint(dlistint_t **head, const int n)
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
