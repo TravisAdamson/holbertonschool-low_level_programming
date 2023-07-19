aadbef2b9be1a8635bdefb8f8731f4b68f5a044b
TravisAdamson <tndadamson@gmail.com>
2023-07-19 10:42:01 -0700

    Small syntax change

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -19,8 +19,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	new->next = *head;
 	new->prev = NULL;
+	new->next = *head;
 	*head = new;
 	return (new);
 }
