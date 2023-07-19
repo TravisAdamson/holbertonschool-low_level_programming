945b10bfe6bde82cedfe018217193cebb79da38b
TravisAdamson <tndadamson@gmail.com>
2023-07-19 12:30:49 -0700

    Fix for p + 1 prev

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -47,5 +47,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	temp->next = previous->next;
 	temp->prev = previous;
 	previous->next = temp;
+	previous->next->prev = temp;
 	return (temp);
 }
