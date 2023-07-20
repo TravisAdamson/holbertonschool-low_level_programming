186cd308eedebad7b32310cf81c4af63197e48f6
TravisAdamson <tndadamson@gmail.com>
2023-07-20 13:43:20 -0700

    Try 1 for 8

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - Deletes the node at index
+ * @head: The list to delete the node from
+ * @index: The location of the node to delete
+ *
+ * Return: 1 if successful and -1 if not
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp;
+	unsigned int i;
+
+	if (!*head || !head)
+		return (-1);
+	temp = *head;
+	if (index == 0)
+	{
+		temp->next->prev = NULL;
+		*head = temp->next;
+		free(temp);
+		return (1);
+	}
+	for (i = 0; i < (index - 1); i++)
+	{
+		temp = temp->next;
+		if (temp == NULL)
+			return (-1);
+	}
+	temp->next->prev = temp->prev;
+	temp->prev->next = temp->next;
+	free(temp);
+	return (1);
+}
