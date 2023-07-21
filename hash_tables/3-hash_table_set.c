46227760101713ddffab82835aa5210559bdad1f
TravisAdamson <tndadamson@gmail.com>
2023-07-21 09:16:13 -0700

    No Idea

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -26,12 +26,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
-	new_node->key = tkey;
-	new_node->value = tvalue;
+	free(new_node->key);
+	free(new_node->value);
 	index = key_index((unsigned const char *)tkey, ht->size);
 	if (!index)
 		return (0);
-	new_node->next = ht->array[index];
+	new_node->key = tkey;
+	new_node->value = tvalue;
+	new_node->next = ht->array[index]->next;
 	ht->array[index] = new_node;
 	return (1);
 }
