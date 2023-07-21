695bf3a30b47044a98ff9b4990aa284d35b1d7cb
TravisAdamson <tndadamson@gmail.com>
2023-07-21 09:19:40 -0700

    changed a null

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -31,7 +31,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	new_node->key = tkey;
 	new_node->value = tvalue;
-	new_node->next = ht->array[index]->next;
+	new_node->next = NULL;
 	ht->array[index] = new_node;
 	return (1);
 }
