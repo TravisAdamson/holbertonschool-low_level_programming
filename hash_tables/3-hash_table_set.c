7b71d3bac33d50a02cbaecf5bdf9491d5bbd162e
TravisAdamson <tndadamson@gmail.com>
2023-07-21 06:08:22 -0700

    Fixed missing ,

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	char *tvalue;
-	hash_node_t *new_node *tnode;
+	hash_node_t *new_node, *tnode;
 
 	if (key == NULL) || (key == "") || (key == '\0')
 		return (0);
