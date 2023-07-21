021550eefca1bcfeebdf21badd9da1ed4e73df6c
TravisAdamson <tndadamson@gmail.com>
2023-07-21 07:21:24 -0700

    Changed from : to ;

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -16,7 +16,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	char *tvalue, *tkey:
+	char *tvalue, *tkey;
 	hash_node_t *new_node, *tnode, *tkeyi;
 
 	if (!key)
