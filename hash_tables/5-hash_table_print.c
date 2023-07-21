63c86531dc2e6588fa236e60429cee05e3ff062f
TravisAdamson <tndadamson@gmail.com>
2023-07-21 16:25:56 -0700

    Try 1 for 5

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints the hash table
+ * @ht: The hash table to be used
+ *
+ * Return: No return
+ */
+void *hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i, size;
+	hash_node_t *temp;
+	char *tkey, *tvalue;
+
+	if (ht != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			temp = ht->array[i];
+			if (temp->key && temp->value)
+			{
+				tkey = temp->key;
+				tvalue = temp->value;
+				printf("%s : %s", tkey, tvalue);
+			}
+			temp = temp->next;
+			if (temp)
+				printf(", ");
+		}
+	}
+}
