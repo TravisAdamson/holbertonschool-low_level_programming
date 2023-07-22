919f6fc9d58d82d678a1dc4a9238207472aa7512
TravisAdamson <tndadamson@gmail.com>
2023-07-22 05:56:50 -0700

    Try 1 for 6

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include "hash_tables.h"
+
+/**
+ * hash_node_delete - Deletes the current node of the table
+ * @temp: The node to be deleted recursively
+ *
+ * Return: No return
+ */
+void hash_node_delete(hash_node_t *temp)
+{
+	hash_node_delete(temp->next);
+	free(temp->key);
+	free(temp->value);
+	free(temp);
+}
+
+/**
+ * hash_table_delete - Deletes the hash table
+ * @ht: The hash table to be deleted
+ *
+ * Return: No return
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *temp;
+
+	if (ht != NULL)
+	{
+		for (i = 0; i < ht->size; i++)
+		{
+			if (ht->array[i] != NULL)
+			{
+				temp = ht->array[i];
+				if (temp != NULL)
+					hash_node_delete(temp);
+			}
+		}
+	}
+}
