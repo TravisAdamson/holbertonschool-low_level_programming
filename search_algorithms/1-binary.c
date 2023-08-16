ec5b658e7bd9f000b87492d3fa2a18473fcd3be2
Travis <6611@holbertonstudents.com>
2023-08-16 05:55:55 -0700

    First binary search push

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,52 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - Implements a binary search for the value
+ * @array: The array to be searched
+ * @size: The size of the array
+ * @value: The value to search for
+ *
+ * Return: The index of the array where value is, or -1 on fail
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t low = 0, high = size - 1, middle = 0;
+
+	print_array(array, low, high);
+	while (low <= high)
+	{
+		middle = (low + high) / 2;
+		if (array[middle] == value)
+			return (middle);
+		else if (array[middle] < value)
+			low = middle + 1;
+		else
+			high = middle - 1;
+		print_array(array, low, high);
+	}
+	return (-1);
+}
+
+/**
+ * print_array - Prints the array being searched
+ * @array: The array to be printed
+ * @size: The size of the given array
+ *
+ * Return: No return
+ */
+void print_array(int *array, size_t low, size_t high)
+{
+	size_t i;
+
+	i = low;
+	printf("Searching in array: ");
+	while (array && i < high)
+	{
+		if (i > low)
+			printf(", ");
+		printf("%d", array[i]);
+		i++;
+	}
+	printf("\n");
+}
--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 
+void print_array(int *array, size_t low, size_t high);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
